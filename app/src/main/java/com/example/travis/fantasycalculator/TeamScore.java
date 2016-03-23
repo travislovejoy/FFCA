b4b2ca72f83cc9e133539f1453ef6e1f204a1691
Travis <travislovejoy@att.net>
2016-03-23 15:06:02 -0700

    Added player scoring descriptions

--- app/src/main/java/com/example/travis/fantasycalculator/TeamScore.java
+++ app/src/main/java/com/example/travis/fantasycalculator/TeamScore.java
@@ -52,7 +52,7 @@ public class TeamScore extends TestDrawer {
     private String STARTER = "starter";
     private String BENCH = "bench";
     private String tag_json_arry = "json_array_req";
-    private String url = "http://192.168.0.6";
+    private String url = "http://192.168.0.7";
     private String url_file = "/getplayerstats.php?";
     private static final String TAG_NAME = "name";
     private static final String TAG_POS = "pos";
@@ -142,60 +142,82 @@ public class TeamScore extends TestDrawer {
 
 
     }
+
+    String rushingDescription(int rush_yards, int rush_tds, String description, boolean RB) {
+        if (rush_yards > 10|| RB) {
+            description += Integer.toString(rush_yards) + " RYDS ";
+        }
+        if (rush_tds >= 1|| RB) {
+            description += Integer.toString(rush_tds) + " RTDS ";
+
+        }
+        return description;
+
+    }
+    String receivingDescription(int rec_yards, int rec_tds, int receptions, String description, boolean WR) {
+        if (rec_yards > 10|| WR) {
+            description += Integer.toString(rec_yards) + " RecYDS ";
+        }
+        if (rec_tds >= 1|| WR) {
+            description += Integer.toString(rec_tds) + " RecTDS ";
+
+        }
+        if(receptions>3||WR) {
+            description+= Integer.toString(receptions)+ " REC ";
+        }
+        return description;
+    }
+
+    String passingDescription(int pass_yards, int pass_tds, String description, boolean QB) {
+        if(pass_yards>25||QB) {
+            description += Integer.toString(pass_yards) + " PYDS ";
+        }
+        if (pass_tds >= 1||QB) {
+            description += Integer.toString(pass_tds) + " PTDS ";
+        }
+        return description;
+    }
     private String getDescription(JSONObject player, String position){
-        if(position.equals("QB")){
-            try {
-                String PY = player.getString("pass_yards");
-                String PTD= player.getString("pass_tds");
-                return PY+ " YDS, "+ PTD+ " TDS";
+        try {
+            int pass_yards = player.getInt("pass_yards");
+            int pass_tds = player.getInt("pass_tds");
+            int rush_yards = player.getInt("rush_yards");
+            int rush_tds = player.getInt("rush_tds");
+            int rec_yards = player.getInt("rec_yards");
+            int rec_tds = player.getInt("rec_tds");
+            int rec = player.getInt("rec");
+            if (position.equals("QB")) {
+                String description="";
+                description= passingDescription(pass_yards, pass_tds, description, true);
+                description= rushingDescription(rush_yards, rush_tds, description, false);
+                return description;
             }
-            catch(JSONException e) {
-                e.printStackTrace();
-
+            else if(position.equals("RB")) {
+                String description="";
+                description= passingDescription(pass_yards, pass_tds, description, false);
+                description= rushingDescription(rush_yards, rush_tds, description, true);
+                description= receivingDescription(rec_yards,rec_tds, rec, description, false);
+                return description;
             }
-
-        }
-        else if(position.equals("RB")){
-            try {
-                String rush_yards = player.getString("rush_yards");
-                String rush_tds = player.getString("rush_tds");
-
-                return rush_yards+ " YDS, "+ rush_tds+ " TDS";
-
+            else if(position.equals("WR")) {
+                String description="";
+                description= passingDescription(pass_yards, pass_tds, description, false);
+                description= rushingDescription(rush_yards, rush_tds, description, false);
+                description= receivingDescription(rec_yards,rec_tds, rec, description, true);
+                return description;
             }
-            catch(JSONException e) {
-                e.printStackTrace();
-
-            }
-
-        }
-        else if(position.equals("WR")){
-            try {
-                int rec_yards = player.getInt("rec_yards");
-                int rec_tds = player.getInt("rec_tds");
-
-                return rec_yards+ " YDS, "+ rec_tds+ " TDS";
-
-
-            }
-            catch(JSONException e) {
-                e.printStackTrace();
-
+            else if(position.equals("Flex")) {
+                String description="";
+                description= passingDescription(pass_yards, pass_tds, description, false);
+                description= rushingDescription(rush_yards, rush_tds, description, false);
+                description= receivingDescription(rec_yards,rec_tds, rec, description, false);
+                return description;
             }
 
         }
-        else if(position.equals("Flex")){
-            try {
-                int rush_yards = player.getInt("rush_yards");
-                int rush_tds = player.getInt("rush_tds");
+        catch(JSONException e) {
+            e.printStackTrace();
 
-                return rush_yards+ " YDS, "+ rush_tds+ " TDS";
-
-            }
-            catch(JSONException e) {
-                e.printStackTrace();
-
-            }
         }
         return "";
     }
